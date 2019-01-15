908f86dd06ed41590f4188fc8f5315a38563d829
venigallanaani <chandra.venigalla@gmail.com>
2019-01-15 15:14:33 -0600

    Added script for multiple timer updates

--- Travian-Tools-work/js/script.js
+++ Travian-Tools-work/js/script.js
@@ -44,3 +44,36 @@ function updPlus(id,sts)
     xmlhttp.open("GET", "ajax/plusAccess.php?id="+id+"&sts="+sts, true);
     xmlhttp.send();
 }
+
+
+function createCountDown(elementId, date) {
+	//https://www.sitepoint.com/community/t/javascript-countdown-timer-looping-for-multiple-rows/270611/5
+    // Set the date we're counting down to
+    var countDownDate = new Date(date).getTime();
+
+    // Update the count down every 1 second
+    var x = setInterval(function() {
+
+      // Get todays date and time
+      var now = new Date().getTime();
+
+      // Find the distance between now an the count down date
+      var distance = countDownDate - now;
+
+      // Time calculations for days, hours, minutes and seconds
+      var days = Math.floor(distance / (1000 * 60 * 60 * 24));
+      var hours = Math.floor((days * 24)+(distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+      var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+      var seconds = Math.floor((distance % (1000 * 60)) / 1000);
+
+      // Display the result in the element with id="demo"
+      document.getElementById(elementId).innerHTML = hours + ":"+ minutes + ":" + seconds;
+
+      // If the count down is finished, write some text 
+      if (distance < 0) {
+        clearInterval(x);
+        document.getElementById(elementId).innerHTML = "00:00:00";
+		document.getElementById(elementId).style.color = "red";
+      }
+    }, 1000);
+}
--- Travian-Tools-work/utilities/time.php
+++ Travian-Tools-work/utilities/time.php
@@ -47,3 +47,36 @@ function countDownTimer($ver,$targetTime,$qualifier){
 <?php     
 }
 ?>
+
+<script>
+function createCountDown(elementId, date) {
+    // Set the date we're counting down to
+    var countDownDate = new Date(date).getTime();
+
+    // Update the count down every 1 second
+    var x = setInterval(function() {
+
+      // Get todays date and time
+      var now = new Date().getTime();
+
+      // Find the distance between now an the count down date
+      var distance = countDownDate - now;
+
+      // Time calculations for days, hours, minutes and seconds
+      var days = Math.floor(distance / (1000 * 60 * 60 * 24));
+      var hours = Math.floor((days * 24)+(distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+      var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+      var seconds = Math.floor((distance % (1000 * 60)) / 1000);
+
+      // Display the result in the element with id="demo"
+      document.getElementById(elementId).innerHTML = hours + ":"+ minutes + ":" + seconds;
+
+      // If the count down is finished, write some text 
+      if (distance < 0) {
+        clearInterval(x);
+        document.getElementById(elementId).innerHTML = "00:00:00";
+		document.getElementById(elementId).style.color = "red";
+      }
+    }, 1000);
+}
+</script>
