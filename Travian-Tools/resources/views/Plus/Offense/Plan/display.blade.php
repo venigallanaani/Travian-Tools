932c5e1009f4dda2ba2fab169583a9f26ce2b46f
venigallanaani <chandra.venigalla@gmail.com>
2019-01-17 22:53:58 -0600

    Updated Offense Planner display

--- Travian-Tools/resources/views/Plus/Offense/Plan/display.blade.php
+++ Travian-Tools/resources/views/Plus/Offense/Plan/display.blade.php
@@ -3,6 +3,14 @@
 @section('content')
 
 	<div class="my-1">
+	@if(count($waves)>0)	
+		<div class="float-md-left p-2 shadow rounded" >
+    		<div class="mx-auto">
+    			<svg id="sankeyChart" width="800" height="300" style="margin:auto"></svg> 			 		
+    		</div>
+		</div>
+	@endif  
+	
 		<div class="d-inline float-md-left col-md-3 py-0 rounded">
 			<table class="m-1 col-md-12 shadow my-1"  style="background-color:#dbeef4">
 				<tr>
@@ -27,14 +35,9 @@
 				</tr>
 			</table>
 		</div>
-		<div class="float-md-left p-2 shadow rounded" >
-    		<div class="mx-auto">
-    			<svg id="sankeyChart" width="800" height="300" style="margin:auto"></svg>    		
-    		</div>
-		</div>	
 	</div>
 	<div class="d-inline float-md-left col-md-12 mx-auto text-center shadow rounded mt-2">
-		<table class="table table-hover table-sm small table-striped m-1">
+		<table class="table table-hover table-sm small table-striped m-1" id="ops">
 			<thead>
     			<tr class="bg-info text-white">
     				<th class="col-md-2">Attacker</th>
@@ -44,7 +47,7 @@
     				<th class="col-md-1">Troops</th>
     				<th class="col-md-2">Land Time</th>
     				<th class="col-md-2">Comments</th>
-    				<th><button class="badge badge-warning" name="addRow"><i class="fas fa-plus"></i></button></th>
+    				<th><button class="badge badge-warning" id="addRow"><i class="fas fa-plus"></i></button></th>
     			</tr>
 			</thead>
 			<tbody>
@@ -52,9 +55,11 @@
 			@php
 				if($wave->type == 'Real'){	$color='text-danger';	}
 				elseif($wave->type == 'Fake'){	$color='text-primary';	}
+				elseif($wave->type == 'Cheif'){	$color='text-warning';	}
+				elseif($wave->type == 'Scout'){	$color='text-success';	}
 				else{	$color='text-dark';	}
 			@endphp			
-    			<tr>
+    			<tr id="{{$wave->id}}">
     				<td class="">
     					<strong><a href="/finder/player/{{$wave->a_player}}/1" target="_blank">{{$wave->a_player}}</a></strong>
     					<a href=""> ({{$wave->a_village}})</a>
@@ -68,11 +73,38 @@
     				<td data-toggle="tooltip" data-placement="top" title="Catapult"><img alt="" src="/images/x.gif" class="units {{$wave->unit}}"></td>
     				<td>{{$wave->landtime}}</td>
     				<td  class="text-left">{{$wave->comments}}</td>
-    				<td><span><button class="badge badge-primary" name="editRow"><i class="far fa-edit"></i></button></span>
-    					<span><button class="badge badge-danger" name="delRow"><i class="far fa-trash-alt"></i></button></span>
+    				<td><span><button class="badge badge-primary" id="editRow"><i class="far fa-edit"></i></button></span>
+    					<span><button class="badge badge-danger" id="delRow"><i class="far fa-trash-alt"></i></button></span>
     				</td>
-    			</tr>	
-		@endforeach	
+    			</tr>
+    			
+    						
+				<tr id="wave-input" style="display:none">
+					<td>X:<input name="a_x" type="text" size="2" placeholder=""/> |Y:<input name="a_y" type="text" size="2" /></td>
+					<td>X:<input name="d_x" type="text" size="2" /> |Y:<input name="d_y" type="text" size="2" /></td>
+					<td><select name="type">
+							<option value="real">Real</option>
+							<option value="fake">Fake</option>
+							<option value="cheif">Cheif</option>
+							<option value="scout">Scout</option>
+							<option value="other">Other</option>
+						</select>
+					</td>
+					<td><input name="waves" type="text" size="2" /></td>
+					<td><select name="units">
+							<option value="1">Catapult</option>
+							<option value="2">Rams</option>
+							<option value="3">Cheif</option>
+							<option value="4">Cavalry</option>
+							<option value="5">Infantry</option>
+						</select>
+					</td>
+					<td><input name="landtime" type="text" size="20" /></td>
+					<td><input name="Comments" type="text" /></td>
+    				<td><span><button class="badge badge-primary" id="saveRow">Save</button></span>
+    				</td>
+				</tr> 
+		@endforeach
 			</tbody>
 		</table>
 	</div>
@@ -82,16 +114,56 @@
 
 <script type="text/javascript">
     $(document).ready(function(){
-        $(".addRow").click(function(){
-            var markup = "<tr><td><input type='checkbox' name='record'></td><td></td><td></td></tr>";
-            $("table tbody").append(markup);
+        $("#addRow").click(function(){
+            var markup = '<tr>'+
+            				'<td>X:<input name="a_x" type="text" size="2" /> |Y:<input name="a_y" type="text" size="2" /></td>'+
+            				'<td>X:<input name="d_x" type="text" size="2" /> |Y:<input name="d_y" type="text" size="2" /></td>'+
+            				'<td><select name="type">'+
+            						'<option value="real">Real</option>'+
+            						'<option value="fake">Fake</option>'+
+            						'<option value="cheif">Cheif</option>'+
+            						'<option value="scout">Scout</option>'+
+            						'<option value="other">Other</option>'+
+            					'</select>'+
+            				'</td>'+
+            				'<td><input name="waves" type="text" size="2" /></td>'+
+            				'<td><select name="units">'+
+            						'<option value="1">Catapult</option>'+
+            						'<option value="2">Rams</option>'+
+            						'<option value="3">Cheif</option>'+
+            						'<option value="4">Cavalry</option>'+
+            						'<option value="5">Infantry</option>'+
+            					'</select>'+
+            				'</td>'+
+            				'<td><input name="landtime" type="text" size="20" /></td>'+
+            				'<td><input name="Comments" type="text" /></td>'+
+            				'<td><span><button class="badge badge-primary" id="saveRow">Save</button></span>'+
+            				'</td>'+
+            			'</tr>';
+
+            
+            $("#ops tbody").append(markup);
         });
-        
-        // Find and remove selected table rows
-        $(".delRow tr").click(function(){
-            $(this).remove();
+    });          
+
+    $(function(){
+    	$('#ops').on('click','#delRow',function(e){            
+
+			var wave= $(this).parents('tr').attr("id");
+
+
+			alert(wave);
+    		//$.get("/"+, function(data){
+    			//alert("Data: " + data + "\nStatus: " + status);
+		    //});
+			
+            //e.preventDefault();
+          	//$(this).parents('tr').remove();
         });
-    });    
+   });
+            
+			        
+  
 </script>
 
 
