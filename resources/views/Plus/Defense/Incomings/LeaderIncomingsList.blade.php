53c48426f586f2a035b0f7a12d3cd2da14a01349
venigallanaani <chandra.venigalla@gmail.com>
2019-02-08 16:35:11 -0600

    Update LeaderIncomingsList.blade.php

--- resources/views/Plus/Defense/Incomings/LeaderIncomingsList.blade.php
+++ resources/views/Plus/Defense/Incomings/LeaderIncomingsList.blade.php
@@ -34,23 +34,52 @@
 			</thead>
 			@foreach($incomings as $incoming)
 				@php
-					if($incoming->ldr_sts=='Attack'){ $incoming = 'table-danger';	}
-					elseif($incoming->ldr_sts=='Fake'){$incoming='table-primary';}
-					elseif($incoming->ldr_sts=='Thinking'){$incoming='table-warning';}					
+					if($incoming->ldr_sts=='Attack'){ $color = 'table-danger';	}
+					elseif($incoming->ldr_sts=='Fake'){$color='table-primary';}
+					elseif($incoming->ldr_sts=='Thinking'){$color='table-warning';}					
 					else{$color='table-white';}					
 				@endphp				
 						
     			<tr class="{{$color}}" id={{$incoming->incid}}>
     				<td><a href="/finder/player/{{$incoming->att_player}}/1"><strong>{{$incoming->att_player}}</strong> 
     						({{$incoming->att_village}})</a></td>
-					<td></td>
+					<td id="100"></td>
     				<td><a href="/finder/player/{{$incoming->def_player}}/1"><strong>{{$incoming->def_player}}</strong>
     						 ({{$incoming->def_village}})</a></td>
 			 		<td>{{$incoming->landTime}}</td>
     				<td>{{$incoming->waves}}</td>
     				<td>{{$incoming->noticeTime}}</td>
     				<td>{{$incoming->hero}}</td>
-    				<td><select><option>0</option>
+    				<td><select id="tsq"><option>0</option>
+    						<option>1</option><option>2</option><option>3</option><option>4</option><option>5</option>
+    						<option>6</option><option>7</option><option>8</option><option>9</option><option>10</option>
+    						<option>11</option><option>12</option><option>13</option><option>14</option><option>15</option>
+    						<option>16</option><option>17</option><option>18</option><option>19</option><option>20</option>    						
+    					</select>
+    				</td>
+    				<td><select name="type">
+    						<option @if($incoming->ldr_sts=='New') selected @endif>New</option>
+    						<option @if($incoming->ldr_sts=='Mark') selected @endif>Mark</option>
+    						<option @if($incoming->ldr_sts=='Attack') selected @endif>Attack</option>
+    						<option @if($incoming->ldr_sts=='Fake') selected @endif>Fake</option>
+    						<option @if($incoming->ldr_sts=='Thinking') selected @endif>Thinking</option>
+    						<option @if($incoming->ldr_sts=='Other') selected @endif>Other</option>
+    					</select>
+    				</td>
+    				<td>{{$incoming->updated_by}}</td>
+    				<td><input type="text" name="comment"></td>
+    			</tr>
+    			<tr class="{{$color}}" id={{$incoming->incid}}>
+    				<td><a href="/finder/player/{{$incoming->att_player}}/1"><strong>{{$incoming->att_player}}</strong> 
+    						({{$incoming->att_village}})</a></td>
+					<td id="100"></td>
+    				<td><a href="/finder/player/{{$incoming->def_player}}/1"><strong>{{$incoming->def_player}}</strong>
+    						 ({{$incoming->def_village}})</a></td>
+			 		<td>{{$incoming->landTime}}</td>
+    				<td>{{$incoming->waves}}</td>
+    				<td>{{$incoming->noticeTime}}</td>
+    				<td>{{$incoming->hero}}</td>
+    				<td><select id="tsq"><option>0</option>
     						<option>1</option><option>2</option><option>3</option><option>4</option><option>5</option>
     						<option>6</option><option>7</option><option>8</option><option>9</option><option>10</option>
     						<option>11</option><option>12</option><option>13</option><option>14</option><option>15</option>
@@ -73,6 +102,44 @@
 		</table>
 		@endif			
 	</div>
-</div>			
-
+</div>	
 @endsection
+
+@push('scripts')
+<script>
+
+	$(document).on('change','#tsq',function(e){
+		e.preventDefault();  
+
+		var wave = $(this).closest("tr");
+		var id= wave.attr("id");
+        var vid = wave.find('td:eq(1)').attr("id");
+		
+        
+		var tsq = wave.find('td:eq(7)').find('select#tsq').val();
+        
+        alert(tsq);
+	});
+
+</script>
+<script>
+	$(document).ready(function(e){
+		e.preventDefault();  
+		
+		$("#list td:nth-child(9)").each(function () {
+			var sts = $(this).find('select#id').val();
+
+			if(sts == 'Thinking'){
+				$(this).parent("tr").toggleClass('table-white table-success');
+			}
+			if(sts == 'Attack'){
+				$(this).parent("tr").toggleClass('table-white table-danger');
+			}
+
+	    });
+		
+	}):
+</script>
+@endpush
+
+
